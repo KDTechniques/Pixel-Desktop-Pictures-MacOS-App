f608d326383d82fe28381c9b5e71a5ec2f082d70
Mr. Kavinda Dilshan <kdtechniques@gmail.com>
2024-12-22 13:08:47 +0530

    Initial Commit

---
+++ Pixel Desktop Pictures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pixel Desktop Pictures
+//
+//  Created by Kavinda Dilshan on 2024-12-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pixel Desktop Pictures/Pixel_Desktop_PicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_Desktop_PicturesApp.swift
+//  Pixel Desktop Pictures
+//
+//  Created by Kavinda Dilshan on 2024-12-22.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_Desktop_PicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
